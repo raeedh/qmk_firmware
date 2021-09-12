502f704ed114b3e084d0ba230cc97df87ef8fc21
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2021-09-12 01:22:27 -0400

    initial keymap/compile

---
+++ keyboards/gmmk/pro/ansi/keymaps/raeedh/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/* Force NKRO on boot up regardless of the setting saved in the EEPROM */
+#define FORCE_NKRO
+
+/* Send up to 12 key press events per scan (not sure if this is actually necessary?) */
+#undef QMK_KEYS_PER_SCAN
+#define QMK_KEYS_PER_SCAN 12
